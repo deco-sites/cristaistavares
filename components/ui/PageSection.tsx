a907af6117be8655a38a7854687fee78a84c4c7e
NetinhoSZ <netinhoskennedy@gmail.com>
2023-09-14 18:14:36 -0300

    Feat: New Feature Added PagesSection

---
+++ components/ui/PageSection.tsx
@@ -0,0 +1,36 @@
+import Image from "apps/website/components/Image.tsx";
+import type { ImageWidget } from "apps/admin/widgets.ts";
+
+export interface Props {
+    cards?: Array<{
+        link: string;
+
+        image: {
+          icon: ImageWidget;
+          alt?: string;
+        };
+        
+      }>;
+}
+
+export default function PagesSection({cards}: Props){
+    return(
+        <section class="w-full h-full flex flex-row flex-wrap items-center justify-center gap-4 my-10">
+            
+            {cards?.map((card) => (
+            <div class="">
+                <a href={card.link}>
+                <Image
+                    src={card.image.icon}
+                    alt={card.image.alt}
+                    width={298}
+                    height={202}
+                    loading="lazy"
+                />
+                </a>
+            </div>
+                ))}
+            
+        </section>
+        )
+}
