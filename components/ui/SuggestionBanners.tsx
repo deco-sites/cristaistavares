304b732cc53fe0d7415e555f0917f9e897a3b45b
NetinhoSZ <netinhoskennedy@gmail.com>
2023-09-18 16:37:06 -0300

    Feat: Create Section SuggestionBanners And Section SectionDescription

---
+++ components/ui/SuggestionBanners.tsx
@@ -0,0 +1,46 @@
+import Image from "apps/website/components/Image.tsx";
+import type { ImageWidget } from "apps/admin/widgets.ts";
+
+export interface Props {
+  title: string;
+  description: string;
+  cards?: Array<{
+    image: {
+      icon: ImageWidget;
+      alt?: string;
+    };
+
+    link?: string;
+  }>;
+}
+
+export default function CategoryList({ title, description, cards }: Props) {
+
+  return (
+    <section class="w-full h-full flex flex-col items-center justify-center py-[50] px-5">
+        <div class="flex flex-col text-center pb-5 ">
+            <div class="">
+                <h1 class="text-xl font-medium">{title}</h1>
+            </div>
+            <div>
+                <p class="text-sm font-bold">{description}</p>
+            </div>
+        </div>
+        <div class="w-full h-full flex flex-wrap items-center justify-center gap-5">
+        {cards?.map((card) => (
+                <div class="flex items-center justify-center w-[295px] h-[368px]">
+                    <a href={card.link}>
+                        <Image
+                        src={card.image.icon}
+                        alt={card.image.alt}
+                        width={296}
+                        height={368}
+                        loading="lazy"
+                        />
+                    </a>
+                </div>
+            ))}
+        </div>
+    </section>
+  );
+}
