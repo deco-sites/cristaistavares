aa2caf733cbe6e71dcaacbc40b6a93fb011a1d8a
@yuri_assuncx <killerkoll2012@gmail.com>
2023-10-04 20:26:36 -0300

    feat: making it testable

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-/// <reference no-default-lib="true"/>
-/// <reference lib="dom" />
-/// <reference lib="deno.ns" />
-/// <reference lib="esnext" />
-
 import { start } from "$fresh/server.ts";
 import plugins from "https://denopkg.com/deco-sites/std@1.22.0/plugins/mod.ts";
 import partytownPlugin from "partytown/mod.ts";
