5dc2b54d89aab7c46f39d8a5224bc435f76b695f
@yuri_assuncx <killerkoll2012@gmail.com>
2023-10-19 07:13:03 -0300

    Update Drawer.tsx

--- components/ui/Drawer.tsx
+++ components/ui/Drawer.tsx
@@ -54,7 +54,7 @@ function Drawer(props: Props) {
         {children}
       </div>
 
-      <aside class="drawer-side h-full z-[999]">
+      <aside class="drawer-side h-full z-50">
         <label for={id} class="drawer-overlay" />
         {!lazy.value && aside}
       </aside>
