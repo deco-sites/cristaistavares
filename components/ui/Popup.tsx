4b9e56448721761a241c1f0f37118b439c4a8534
@yuri_assuncx <killerkoll2012@gmail.com>
2023-10-30 13:39:34 -0300

    chore & fix: versions updated

--- components/ui/Popup.tsx
+++ components/ui/Popup.tsx
@@ -218,6 +218,8 @@ export default function Popup({
                     {emailInput}
                     <div class="w-full flex items-center gap-0.5 py-2">
                       <input
+                        id="popup-checkbox"
+                        aria-label="accept terms"
                         type="checkbox"
                         class="checkbox border-black w-[13px] h-[13px] bg-white ml-1"
                       />
