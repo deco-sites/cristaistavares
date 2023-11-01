d4189d3e2c2234a1d39d939e4ec25e8ce405ac35
@yuri_assuncx <killerkoll2012@gmail.com>
2023-11-01 20:14:06 -0300

    feat: Black Friday page improved

--- components/ui/LPCategoryCards.tsx
+++ components/ui/LPCategoryCards.tsx
@@ -1,3 +1,4 @@
+import Image from "apps/website/components/Image.tsx";
 import type { HTMLWidget } from "apps/admin/widgets.ts";
 import type { ImageWidget } from "apps/admin/widgets.ts";
 
@@ -5,6 +6,7 @@ export interface Props {
   title?: HTMLWidget;
   subTitle?: HTMLWidget;
   cards?: {
+    link: string;
     image: {
       link: ImageWidget;
       description: string;
@@ -16,9 +18,9 @@ export interface Props {
 
 export default function LPCategoryCards({ title, subTitle, cards }: Props) {
   return (
-    <div class="w-full h-full bg-black">
-      <section class="w-full h-full flex flex-col items-center justify-center">
-        <div class="w-full text-center mt-4">
+    <section class="w-full h-full container max-w-[1240px] py-5 px-4">
+      <div class="w-full h-full flex flex-col items-center justify-center">
+        <div class="flex flex-col w-full text-center gap-2">
           <h1
             class="text-2xl text-white font-bold"
             dangerouslySetInnerHTML={{ __html: title || "" }}
@@ -28,23 +30,26 @@ export default function LPCategoryCards({ title, subTitle, cards }: Props) {
             dangerouslySetInnerHTML={{ __html: subTitle || "" }}
           />
         </div>
-        <div class="w-full h-full flex flex-wrap items-center justify-center my-4 gap-4">
+        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 w-full h-full items-center justify-center gap-4">
           {cards?.map((item) => (
-            <div class="flex flex-col items-center justify-center">
-              <img
+            <a
+              href={item.link}
+              class="flex flex-col items-center justify-center"
+            >
+              <Image
                 src={item.image.link}
-                width={140}
-                height={150}
-                class="rounded-lg w-[140px] h-[150px]"
-                alt={item.image.description || "Cards Banner"}
+                width={191}
+                height={191}
+                class="rounded-lg"
+                alt={item.image.description}
               />
-              <p class="w-[130px] h-full text-lg text-center text-white font-semibold">
+              <p class="w-[130px] min-h-[58px] text-lg text-center text-white font-semibold">
                 {item.titleCategory}
               </p>
-            </div>
+            </a>
           ))}
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 }
