01e6c3a666c682035c11e83e403d20b62dfdb3ea
@yuri_assuncx <killerkoll2012@gmail.com>
2023-11-06 14:49:46 -0300

    style: adding more margin-top on tablet

--- components/ui/SectionDescription.tsx
+++ components/ui/SectionDescription.tsx
@@ -15,7 +15,7 @@ export default function SectionDescription(
     <section
       class={`${
         hasMostPaddingBottom && "pb-4"
-      } h-full flex flex-col items-center justify-center text-center gap-2 mt-[30px] px-4 lg:px-0`}
+      } h-full flex flex-col items-center justify-center text-center gap-2 mt-[30px] md:mt-10 lg:mt-[30px] px-4 lg:px-0`}
     >
       <div>
         <h2
