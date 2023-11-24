554535f812319ee832935b943dfe2cd66be18e0f
@yuri_assuncx <killerkoll2012@gmail.com>
2023-11-24 12:26:01 -0300

    fix: image size fixed

--- components/minicart/common/CartItem.tsx
+++ components/minicart/common/CartItem.tsx
@@ -67,7 +67,8 @@ function CartItem(
     >
       <a href={detailUrl} class="contents shadow-sm rounded-lg">
         <Image
-          {...image}
+          src={image?.src.replace("55-55", "96-96")}
+          alt={image?.alt}
           style={{ aspectRatio: "1" }}
           width={96}
           height={96}
