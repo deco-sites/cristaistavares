eeacc5a49e1ffd8405c4b5b75d83cff05ec419b2
@yuri_assuncx <killerkoll2012@gmail.com>
2023-12-11 18:23:53 -0300

    feat: new features available on Home

--- sections/Miscellaneous/Mailbiz.tsx
+++ sections/Miscellaneous/Mailbiz.tsx
@@ -1,46 +1,22 @@
 const Mailbiz = () => {
-  const jsonLdData = {
-    "@context": "https://schema.org",
-    "@type": "Corporation",
-    "name": "Cristais Tavares Decoracao Em Murano Eireli",
-    "alternateName":
-      "Cristais Tavares - Maior loja de decoração em murano direto de fábrica.",
-    "url": "https://www.cristaistavares.com.br/",
-    "logo":
-      `https://www.cristaistavares.com.br/live/invoke/website/loaders/image.ts?src=${
-        encodeURIComponent(
-          "https://cristaistavares.vtexassets.com/arquivos/logo-cristais-tavares-svg.svg",
-        )
-      }&fit=cover&width=252&height=32&__frsh_c=09ca21774e269fa5baf6f5f5895786154727ccbc`,
-    "contactPoint": [{
-      "@type": "ContactPoint",
-      "telephone": "(47) 3232-1875",
-      "contactType": "sales",
-      "areaServed": "BR",
-      "availableLanguage": "Portuguese",
-    }, {
-      "@type": "ContactPoint",
-      "telephone": "(47) 99115-1995",
-      "contactType": "customer service",
-      "areaServed": "BR",
-      "availableLanguage": "Portuguese",
-    }],
-    "sameAs": [
-      "https://www.facebook.com/cristaistavares/",
-      "https://www.instagram.com/cristaistavares/",
-      "https://br.pinterest.com/cristaistavares/",
-      "https://www.cristaistavares.com.br/",
-      "https://www.youtube.com/channel/UCRJ_WTKeL6jtu2C1B-5vIhw",
-      "https://br.linkedin.com/company/cristais-tavares",
-      "https://twitter.com/CristaisTavares",
-    ],
-  };
+  const scriptCode = `
+    (function(m, a, i, l, b, z, j, s) {
+      if (m[z]) return;
+      m[z] = {
+        id: b,
+        ready: 0
+      };
+      z = a.createElement(i);
+      j = a.getElementsByTagName(i)[0];
+      z.async = 1;
+      z.src = l;
+      j.parentNode.insertBefore(z, j);
+    })(window, document, 'script', 'https://d3eq1zq78ux3cv.cloudfront.net/static/scripts/integration.min.js', '65521c0c7058d778ef9480cd', 'MailbizIntegration');
+  `;
 
   return (
     <>
-      <script async type="application/ld+json">
-        {JSON.stringify(jsonLdData)}
-      </script>
+      <script async dangerouslySetInnerHTML={{ __html: scriptCode }} />
     </>
   );
 };
---
+++ sections/Miscellaneous/MailbizJSON.tsx
@@ -0,0 +1,48 @@
+const MailbizJSON = () => {
+  const jsonLdData = {
+    "@context": "https://schema.org",
+    "@type": "Corporation",
+    "name": "Cristais Tavares Decoracao Em Murano Eireli",
+    "alternateName":
+      "Cristais Tavares - Maior loja de decoração em murano direto de fábrica.",
+    "url": "https://www.cristaistavares.com.br/",
+    "logo":
+      `https://www.cristaistavares.com.br/live/invoke/website/loaders/image.ts?src=${
+        encodeURIComponent(
+          "https://cristaistavares.vtexassets.com/arquivos/logo-cristais-tavares-svg.svg",
+        )
+      }&fit=cover&width=252&height=32&__frsh_c=09ca21774e269fa5baf6f5f5895786154727ccbc`,
+    "contactPoint": [{
+      "@type": "ContactPoint",
+      "telephone": "(47) 3232-1875",
+      "contactType": "sales",
+      "areaServed": "BR",
+      "availableLanguage": "Portuguese",
+    }, {
+      "@type": "ContactPoint",
+      "telephone": "(47) 99115-1995",
+      "contactType": "customer service",
+      "areaServed": "BR",
+      "availableLanguage": "Portuguese",
+    }],
+    "sameAs": [
+      "https://www.facebook.com/cristaistavares/",
+      "https://www.instagram.com/cristaistavares/",
+      "https://br.pinterest.com/cristaistavares/",
+      "https://www.cristaistavares.com.br/",
+      "https://www.youtube.com/channel/UCRJ_WTKeL6jtu2C1B-5vIhw",
+      "https://br.linkedin.com/company/cristais-tavares",
+      "https://twitter.com/CristaisTavares",
+    ],
+  };
+
+  return (
+    <>
+      <script async type="application/ld+json">
+        {JSON.stringify(jsonLdData)}
+      </script>
+    </>
+  );
+};
+
+export default MailbizJSON;
