a870abf40d9fecb733ecbac63c0532ab4061189f
@yuri_assuncx <killerkoll2012@gmail.com>
2024-01-12 15:07:54 -0300

    feat: adding Widde script to GlobalTags

--- components/GlobalTags.tsx
+++ components/GlobalTags.tsx
@@ -49,6 +49,13 @@ function GlobalTags() {
         `,
         }}
       />
+
+      <script
+        type="text/javascript"
+        id=""
+        async
+        src="https://cdn.widde.io/widde.1.1.0.js?v=1.0"
+      />
     </Head>
   );
 }
