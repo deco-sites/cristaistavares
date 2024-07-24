ffd664480e202b164f75b91e09e58bc3af2dbd29
@yuri_assuncx <killerkoll2012@gmail.com>
2024-07-24 13:53:11 -0300

    fix (Header): scroll script removed

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -7,7 +7,6 @@ import Navbar from "./Navbar.tsx";
 import { headerHeight } from "./constants.ts";
 import { usePlatform } from "$store/sdk/usePlatform.tsx";
 import { FnContext } from "deco/types.ts";
-import { useScript } from "deco/hooks/useScript.ts";
 
 export interface NavItem {
   label: string;
@@ -55,56 +54,33 @@ function Header({
   const platform = usePlatform();
   const searchbar = { ..._searchbar };
 
-  function handleScroll() {
-    self.addEventListener("scroll", () => {
-      const scrollY = self.scrollY;
-      const navbar = document.getElementById("navbar");
-      const campaignTimer = document.getElementById("campaign-timer");
-
-      if (scrollY > 60) {
-        navbar?.classList?.add("fixed");
-        campaignTimer?.classList?.add("hidden");
-      } else {
-        navbar?.classList?.remove("fixed");
-        campaignTimer?.classList?.remove("hidden");
-      }
-    });
-  }
-
   return (
-    <>
-      <header
-        style={{
-          height: headerHeight,
-        }}
-        class="z-50 w-full h-full relative"
+    <header
+      style={{
+        height: headerHeight,
+      }}
+      class="z-50 w-full h-full relative"
+    >
+      <Drawers
+        menu={{ items: navItems }}
+        searchbar={searchbar}
+        platform={platform}
       >
-        <Drawers
-          menu={{ items: navItems }}
-          searchbar={searchbar}
-          platform={platform}
+        <div
+          id="navbar"
+          class="w-full bg-base-100 z-50 drop-shadow-lg"
         >
-          <div
-            id="navbar"
-            class="w-full bg-base-100 z-50 drop-shadow-lg"
-          >
-            <LoadingProgressBar />
-            {flagOnTop && <Alert {...flagOnTop} />}
-            <Navbar
-              items={navItems}
-              searchbar={searchbar}
-              logo={logo}
-              device={device}
-            />
-          </div>
-        </Drawers>
-      </header>
-
-      <script
-        type="module"
-        dangerouslySetInnerHTML={{ __html: useScript(handleScroll) }}
-      />
-    </>
+          <LoadingProgressBar />
+          {flagOnTop && <Alert {...flagOnTop} />}
+          <Navbar
+            items={navItems}
+            searchbar={searchbar}
+            logo={logo}
+            device={device}
+          />
+        </div>
+      </Drawers>
+    </header>
   );
 }
 
