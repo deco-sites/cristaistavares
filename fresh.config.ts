41bc6157a398c5adbe766e0ebb36229b864abd81
@yuri_assuncx <killerkoll2012@gmail.com>
2024-07-26 14:30:54 -0300

    chore: partytown plugins removed from website

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "$fresh/src/server/defines.ts";
 import plugins from "deco-sites/std/plugins/mod.ts";
-import partytownPlugin from "partytown/mod.ts";
 import decoManifest from "./manifest.gen.ts";
 import tailwind from "./tailwind.config.ts";
 
@@ -11,7 +10,6 @@ export default defineConfig({
       // deno-lint-ignore no-explicit-any
       tailwind: tailwind as any,
     }),
-    partytownPlugin(),
   ],
   render: (ctx, render) => {
     ctx.lang = "pt-BR";
